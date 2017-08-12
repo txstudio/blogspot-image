3f5a45a5f5bf62aad2a47704b67f688b85ad2fa5
Shi Hao Wang <st890409@hotmail.com>
2017-08-12 20:10:22 +0800

    搜尋時隱藏最新文章

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 $(function(){
-	console.log("test from github js");
+	var _href = "";
+	var _matchPattern = "/search/label/";
+	
+	if(location.href.match(_matchPattern) == undefined)
+	{
+	}
+	else
+	{
+		$("#featured_post").css("display","none");
+	}
 });

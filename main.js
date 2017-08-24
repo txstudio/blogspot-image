91c4acd83fe0ac9cf9ca87a1bb54f68dc2af1991
Shi Hao Wang <st890409@hotmail.com>
2017-08-24 10:39:21 +0800

    沒有縮圖的文章提供預設圖片

--- main.js
+++ main.js
@@ -9,4 +9,12 @@ $(function(){
 	{
 		$("#featured_post").css("display","none");
 	}
+	
+	//沒有縮圖的文章提供預設圖片
+	$(".post-outer>.post-wrapper").each(function(){ 
+		if($(this).hasClass("no-image") === true) { 
+			$(this).removeClass("no-image").addClass("image");
+		}; 
+	});
 });
+
